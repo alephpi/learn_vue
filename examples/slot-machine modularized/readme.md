cb75609150348a1b79746429c2fde5d69e9fbc5f
润心 <maosicheng98@gmail.com>
2022-08-12 00:19:52 +0200

    decompose slot-machine

---
+++ examples/slot-machine modularized/readme.md
@@ -0,0 +1,1 @@
+A modularized slot-machine with optional and HTML api of vue3 by CDN (without build).
--- toponym_slot_machine/app_new.js
+++ toponym_slot_machine/app_new.js
@@ -1,6 +1,6 @@
 import typonym_json from "./data_structured.json" assert { type: "json" };
-var roll = () => {
+export function roll() {
   let keys = Object.keys(typonym_json);
   console.log(keys);
   console.log("hello world");
-};
+}
