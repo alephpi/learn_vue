e4e3bc017d0e7ab8c70a4b7dedb9c94b24b43c86
润心 <maosicheng98@gmail.com>
2022-08-15 09:55:46 +0200

    chore: update

--- toponym_slot_machine/app_new.js
+++
@@ -1,6 +0,0 @@
-import typonym_json from "./data_structured.json" assert { type: "json" };
-export function roll() {
-  let keys = Object.keys(typonym_json);
-  console.log(keys);
-  console.log("hello world");
-}
